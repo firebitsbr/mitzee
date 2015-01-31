a3acc885c954dd9e9bc268d0afe9a1f43978a6b8
Marius <marrius9876@gmail.com>
2015-01-31 17:29:43 -0500

    added header for httphdr.h file

--- buflea/httphdr.cpp
+++ buflea/httphdr.cpp
@@ -1,13 +1,27 @@
+/**
+# Copyright (C) 2012-2014 Chincisan Octavian-Marius(mariuschincisan@gmail.com) - coinscode.com - N/A
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+*/
 
 
 #include "context.h"
 
 
-
-
-//----------------------------------------------------------------------------
-//custom. All HTTP header parsers are crappy and  slowwwwwwww......and crash
-// so better reinvent the wheel....
+//-------------------------------------------------------------------------------------
+// All crappy HTTP header parsers I test either crashes either suck in
+// resource allocation and handlings strings.
+// .. better reinvent the wheel.... (not so profi, but very fast and did not crash yet)
 
 const char* SocksHdr::_host = "Host: ";
 const char* SocksHdr::_prox = "Proxy-Connection: ";
